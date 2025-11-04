e8dd710b42c1d5a3bea008cc79b75bdd4d8979ef
idekakdj <paul.chapman2201@gmail.com>
2025-11-04 10:44:43 -0500

    fdsf

--- Survivors.java
+++ Survivors.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
 /**
  * Write a description of class Survivors here.
  * 
@@ -10,9 +10,14 @@ public abstract class Survivors extends Actor
 {
     protected final int startHP = 100;
     protected int hp;
+    private final int DETECTION = 100;
     public void act()
     {
         hp = startHP;
+        List<Zombies> nearbyZombies = this.getObjectsInRange(DETECTION, Zombies.class);
+        for(Zombies z : nearbyZombies){
+            if
+        }
     }
     public void takeDamage(int damage){
         hp = hp - damage;
