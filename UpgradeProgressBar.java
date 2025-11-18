77dcfbfbe203530792d3add6eb4ca455af0a7d93
idekakdj <paul.chapman2201@gmail.com>
2025-11-18 10:37:58 -0500

    fd

--- UpgradeProgressBar.java
+++ UpgradeProgressBar.java
@@ -1,52 +1,124 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
+import greenfoot.*;
+/*
  * 
- * @author Paul
+ * @author Paul assisted by Claude
  * 
- * 
- * Takes in parameters to be reusable and make different upgrade progress bars.
- * width is the width of the bar
- * height is the height of the bar
- * color is the main portion of the bar's color
- * borderWidth is how many px you want your border ( won't draw if = 0)
- * borderColor sets the border color
- * text is the text on the progress bar
  */
 public class UpgradeProgressBar extends Actor
 {
     private int width;
     private int height;
-    private Color color;
+    private Color fillColor;
+    private Color emptyColor;
     private int borderWidth;
     private Color borderColor;
     private String text;
-    GreenfootImage rect;
-    public UpgradeProgressBar(int width, int height, Color color, int borderWidth, Color borderColor, String text){
+    private int targetScore = 3000;
+    private int currentScore = 0;
+    private boolean isComplete = false;
+    private int glowCounter = 0;
+    private GreenfootImage rect;
+    
+    public UpgradeProgressBar(int width, int height, Color fillColor, Color emptyColor, int borderWidth, Color borderColor, String text){
         this.width = width;
         this.height = height;
-        this.color = color;
+        this.fillColor = fillColor;
+        this.emptyColor = emptyColor;
         this.borderWidth = borderWidth;
         this.borderColor = borderColor;
         this.text = text;
         drawBar();
+        setImage(rect);
     }
-    public void drawBar(){
-        rect = new GreenfootImage(width, height);
-        if(borderWidth > 0){
-            rect.setColor(borderColor);
-            rect.fillRect(0,0,width,height);
-            
-            rect.setColor(color);
-            rect.fillRect(borderWidth, borderWidth,width - (borderWidth * 2), height - (borderWidth * 2));
-        } else{
-            rect.setColor(color);
-            rect.fillRect(0, 0,width, height);
-        }
-        
-    }
+    
     public void act()
     {
+        updateProgress();
         
+        if (isComplete) {
+            glow();
+        }
+    }
+    
+    private void updateProgress(){
+        if (getWorld() instanceof GameWorld) {
+            GameWorld world = (GameWorld) getWorld();
+            ScoreTracker tracker = world.getObjects(ScoreTracker.class).get(0);
+            
+            if (tracker != null) {
+                currentScore = tracker.numRegular * 10 + tracker.numPenguin * 20 + 
+                              tracker.numSpecial * 20 + tracker.numBoss * 100;
+                
+                if (currentScore >= targetScore && !isComplete) {
+                    isComplete = true;
+                }
+                
+                drawBar();
+                setImage(rect);
+            }
+        }
+    }
+    
+    private void drawBar(){
+        rect = new GreenfootImage(width, height);
+        
+        if(borderWidth > 0){
+            rect.setColor(borderColor);
+            rect.fillRect(0, 0, width, height);
+            
+            rect.setColor(emptyColor);
+            rect.fillRect(borderWidth, borderWidth, width - (borderWidth * 2), height - (borderWidth * 2));
+            
+            int progressWidth = (int)((currentScore / (double)targetScore) * (width - (borderWidth * 2)));
+            if (progressWidth > width - (borderWidth * 2)) {
+                progressWidth = width - (borderWidth * 2);
+            }
+            
+            rect.setColor(fillColor);
+            rect.fillRect(borderWidth, borderWidth, progressWidth, height - (borderWidth * 2));
+        } else {
+            rect.setColor(emptyColor);
+            rect.fillRect(0, 0, width, height);
+            
+            int progressWidth = (int)((currentScore / (double)targetScore) * width);
+            if (progressWidth > width) {
+                progressWidth = width;
+            }
+            
+            rect.setColor(fillColor);
+            rect.fillRect(0, 0, progressWidth, height);
+        }
+        
+        rect.setColor(Color.WHITE);
+        rect.setFont(new Font("Arial", 16));
+        
+        GreenfootImage textImg = new GreenfootImage(text + ": " + currentScore + "/" + targetScore, 16, Color.WHITE, new Color(0,0,0,0));
+        int textX = (width - textImg.getWidth()) / 2;
+        int textY = (height + textImg.getHeight()) / 2 - 3;
+        rect.drawString(text + ": " + currentScore + "/" + targetScore, textX, textY);
+    }
+    
+    private void glow(){
+        glowCounter++;
+        
+        int transparency = (int)(128 + 127 * Math.sin(glowCounter * 0.1));
+        
+        GreenfootImage glowImg = new GreenfootImage(width + 10, height + 10);
+        glowImg.setColor(new Color(255, 255, 0, transparency));
+        glowImg.fillOval(0, 0, width + 10, height + 10);
+        
+        getWorld().getBackground().drawImage(glowImg, getX() - width/2 - 5, getY() - height/2 - 5);
+        
+        getWorld().getBackground().drawImage(rect, getX() - width/2, getY() - height/2);
+    }
+    
+    public boolean isUpgradeReady() {
+        return isComplete;
+    }
+    
+    public void reset() {
+        currentScore = 0;
+        isComplete = false;
+        glowCounter = 0;
     }
 }
